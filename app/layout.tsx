eae87f773ee532f2ac09cf6db7726a5042985a09
Tanvir <tanvirmern@gmail.com>
2026-03-08 22:42:50 +0600

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       suppressHydrationWarning
       className={cn("antialiased", fontMono.variable, "font-sans", fontSans.variable)}
     >
-      <body>
+      <body suppressHydrationWarning>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
